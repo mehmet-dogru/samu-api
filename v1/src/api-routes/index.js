8585b2723908fb7f3f4728fae5c5ce56aa289194
Mehmet Doğru <mdogru685@gmail.com>
2023-05-13 00:45:57 +0300

    feat: add graduated routes

--- v1/src/api-routes/index.js
+++ v1/src/api-routes/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require("./user.route");
 const postRoutes = require("./post.route");
 const studentRoutes = require("./student.route");
+const graduatedRoutes = require("./graduated.route");
 
 module.exports = {
   userRoutes,
   postRoutes,
   studentRoutes,
+  graduatedRoutes,
 };
