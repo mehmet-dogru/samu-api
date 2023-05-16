1024fbaf99bef92c4059c2ffc5aa090d2b96ed95
Mehmet Doğru <mdogru685@gmail.com>
2023-05-16 16:59:49 +0300

    fix: add company import

--- v1/src/api-routes/index.js
+++ v1/src/api-routes/index.js
@@ -2,10 +2,12 @@ const userRoutes = require("./user.route");
 const postRoutes = require("./post.route");
 const studentRoutes = require("./student.route");
 const graduatedRoutes = require("./graduated.route");
+const companyRoutes = require("./company.route");
 
 module.exports = {
   userRoutes,
   postRoutes,
   studentRoutes,
   graduatedRoutes,
+  companyRoutes,
 };
--- v1/src/app.js
+++ v1/src/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const config = require("./config/index");
 const loaders = require("./loaders/index");
-const { userRoutes, postRoutes, studentRoutes, graduatedRoutes } = require("./api-routes");
+const { userRoutes, postRoutes, studentRoutes, graduatedRoutes, companyRoutes } = require("./api-routes");
 const errorHandler = require("./middlewares/error-handler.middleware");
 const ApiError = require("./responses/error.response");
 const httpStatus = require("http-status");
@@ -20,6 +20,7 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 app.use("/student", studentRoutes);
 app.use("/graduated", graduatedRoutes);
+app.use("/company", companyRoutes);
 
 app.use((req, res, next) => {
   next(new ApiError("ENDPOINT NOT FOUND", httpStatus.BAD_REQUEST));
