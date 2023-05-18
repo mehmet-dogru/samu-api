466fcf40c7a3582dfda9b446526d18e5d5c98177
Mehmet Doğru <mdogru685@gmail.com>
2023-05-18 20:08:12 +0300

    feat: graduation project modul

---
+++ v1/src/controllers/graduation-project.controller.js
@@ -0,0 +1,31 @@
+const httpStatus = require("http-status");
+const graduationProjectService = require("../services/graduation-project.service");
+const ApiError = require("../responses/error.response");
+const successResponse = require("../responses/success.response");
+
+class GraduationProjectController {
+  async create(req, res, next) {
+    try {
+      const project = await graduationProjectService.create({
+        ...req.body,
+      });
+
+      successResponse(res, httpStatus.CREATED, project);
+    } catch (error) {
+      return next(new ApiError(error.message, httpStatus.BAD_REQUEST));
+    }
+  }
+
+  async list(req, res, next) {
+    try {
+      const { page = 1, limit = 10 } = req.query;
+      const projects = await graduationProjectService.list(page, limit);
+
+      successResponse(res, httpStatus.OK, projects);
+    } catch (error) {
+      return next(new ApiError(error.message, httpStatus.BAD_REQUEST));
+    }
+  }
+}
+
+module.exports = new GraduationProjectController();
---
+++ v1/src/services/graduation-project.service.js
@@ -0,0 +1,25 @@
+const BaseService = require("./base.service");
+const BaseModel = require("../models/GraduationProject");
+
+class GraduationProjectService extends BaseService {
+  constructor() {
+    super(BaseModel);
+  }
+
+  list(page, limit, where) {
+    const projects = BaseModel.find(where || {})
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .populate({
+        path: "students",
+        select: "firstName lastName profileImage studentId department",
+      })
+      .populate({
+        path: "supervisor",
+        select: "firstName lastName profileImage department",
+      });
+    return projects;
+  }
+}
+
+module.exports = new GraduationProjectService();
