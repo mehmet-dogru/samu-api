b61ed5e9ed50636980e9ec7b5628c77857e3a4b3
Mehmet Doğru <mdogru685@gmail.com>
2023-05-18 21:10:34 +0300

    feat: add roles academician for gra. project

--- v1/src/api-routes/graduation-project.route.js
+++ v1/src/api-routes/graduation-project.route.js
@@ -6,8 +6,16 @@ const validate = require("../middlewares/validate.middleware");
 const validationSchema = require("../validations/graduation-project.validation");
 const authenticate = require("../middlewares/authenticate.middleware");
 const authorization = require("../middlewares/authorization.middleware");
+const ROLES = require("../references/role.reference");
 
-router.route("/").post(authenticate, validate(validationSchema.createGraduationProjectSchema), graduationProjectController.create);
+router
+  .route("/")
+  .post(
+    authenticate,
+    authorization([ROLES.ACADEMICIAN]),
+    validate(validationSchema.createGraduationProjectSchema),
+    graduationProjectController.create
+  );
 router.route("/").get(authenticate, graduationProjectController.list);
 
 module.exports = router;
