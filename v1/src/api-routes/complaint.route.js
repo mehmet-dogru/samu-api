c55932bdc9020402911a142fc3a8429d7bb935b6
Mehmet Doğru <mdogru685@gmail.com>
2023-05-21 14:02:47 +0300

    feat: update visibility and listAll

--- v1/src/api-routes/complaint.route.js
+++ v1/src/api-routes/complaint.route.js
@@ -6,9 +6,19 @@ const validate = require("../middlewares/validate.middleware");
 const validationSchema = require("../validations/complaint.validation");
 const authenticate = require("../middlewares/authenticate.middleware");
 const authorization = require("../middlewares/authorization.middleware");
+const ROLES = require("../references/role.reference");
 
 router.route("/").post(authenticate, validate(validationSchema.createComplaintSchema), complaintController.create);
 router.route("/").get(authenticate, complaintController.list);
+router.route("/list-all").get(authenticate, authorization([ROLES.ACADEMICIAN, ROLES.ADMIN]), complaintController.listAll);
 router.route("/update-status/:id").put(authenticate, validate(validationSchema.updateComplaintStatusSchema), complaintController.updateStatus);
+router
+  .route("/update-visibility/:id")
+  .put(
+    authenticate,
+    authorization([ROLES.ACADEMICIAN, ROLES.ADMIN]),
+    validate(validationSchema.updateComplaintVisibilitySchema),
+    complaintController.updateVisibility
+  );
 
 module.exports = router;
