cfc035eb2e6da7856d8cc463837f1bea47b34891
Mehmet Doğru <mdogru685@gmail.com>
2023-05-21 13:21:27 +0300

    fix: add admin role and create endpoint name

--- v1/src/api-routes/post.route.js
+++ v1/src/api-routes/post.route.js
@@ -6,8 +6,12 @@ const validate = require("../middlewares/validate.middleware");
 const validationSchema = require("../validations/post.validation");
 const authenticate = require("../middlewares/authenticate.middleware");
 const authorization = require("../middlewares/authorization.middleware");
+const ROLES = require("../references/role.reference");
 
-router.route("/create").post(authenticate, validate(validationSchema.createPostSchema), postController.create);
+router
+  .route("/")
+  .post(authenticate, authorization([ROLES.ACADEMICIAN, ROLES.ADMIN]), validate(validationSchema.createPostSchema), postController.create);
+
 router.route("/").get(authenticate, postController.list);
 
 router.route("/create-comment/:postId").post(authenticate, validate(validationSchema.commentValidation), postController.createComment);
