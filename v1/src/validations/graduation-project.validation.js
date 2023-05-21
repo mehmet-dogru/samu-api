cb7e43ef68a7d049755ce313ea1da8784237e991
Mehmet Doğru <mdogru685@gmail.com>
2023-05-21 15:28:34 +0300

    feat: update and delete graduation project

--- v1/src/validations/graduation-project.validation.js
+++ v1/src/validations/graduation-project.validation.js
@@ -7,12 +7,11 @@ const createGraduationProjectSchema = Joi.object({
   students: Joi.array().items(Joi.string().length(24).hex().required()),
 });
 
-// const updateGraduationProjectSchema = Joi.object({
-//   name: Joi.string().required(),
-//   date: Joi.date().required(),
-//   description: Joi.string().min(5),
-//   students: Joi.array().items(Joi.string().length(24).hex().required()),
-//   supervisor: Joi.array().items(Joi.string().length(24).hex().required()),
-// });
+const updateGraduationProjectSchema = Joi.object({
+  name: Joi.string().required(),
+  date: Joi.date().required(),
+  description: Joi.string().min(5),
+  students: Joi.array().items(Joi.string().length(24).hex().required()),
+});
 
-module.exports = { createGraduationProjectSchema };
+module.exports = { createGraduationProjectSchema, updateGraduationProjectSchema };
