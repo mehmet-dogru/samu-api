d37ae3f830b0b45a2398bdee4a48032d2a4991dc
Mehmet Doğru <mdogru685@gmail.com>
2023-05-25 00:34:35 +0300

    feat: add all posts route

--- v1/src/api-routes/post.route.js
+++ v1/src/api-routes/post.route.js
@@ -12,7 +12,7 @@ router
   .route("/")
   .post(authenticate, authorization([ROLES.ACADEMICIAN, ROLES.ADMIN]), validate(validationSchema.createPostSchema), postController.create);
 
-router.route("/").get(authenticate, postController.list);
-
+router.route("/").get(authenticate, authorization([ROLES.ACADEMICIAN, ROLES.STUDENT]), postController.list);
+router.route("/all").get(authenticate, authorization([ROLES.ADMIN]), postController.listAll);
 
 module.exports = router;
