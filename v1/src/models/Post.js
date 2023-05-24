1479b7070c8c9fc09bd284a44b4a2b9e2bfeb23a
Mehmet Doğru <mdogru685@gmail.com>
2023-05-25 00:35:54 +0300

    feat: add depament field

--- v1/src/models/Post.js
+++ v1/src/models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema(
   {
-    title : { type: String, required: true, min: 1, max: 100 },
+    title: { type: String, required: true, min: 1, max: 100 },
     content: { type: String, required: true, min: 1, max: 500 },
     imageUrl: { type: String, trim: true },
     createdAt: { type: Date, default: Date.now },
@@ -10,6 +10,10 @@ const PostSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
+    },
+    department: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Department",
     },
   },
   { timestamps: false, versionKey: false }
